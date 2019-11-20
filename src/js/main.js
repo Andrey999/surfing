721dcb75d9abfd34f464aca82d4bfdce826d5e3e
Gromov Andrey <gromovandrey1985@gmail.com>
2019-11-20 15:34:06 +0300

    add slick-carousel to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 window.onload = function() {
-  console.log('work');
+
+  // slick  slider
+  $('.header__slider').slick({
+    infinite: true,
+    prevArrow: '<img class="slider__arrows slider__arrows-left" src="src/img/arrow-left.svg" alt="arrow-left">',
+    nextArrow: '<img class="slider__arrows slider__arrows-right" src="src/img/arrow-right.svg" alt="arrow-right">',
+  });
 };
