2eb3fc3371259e585c12be51d7ac9a5b99939621
Gromov Andrey <gromovandrey1985@gmail.com>
2019-11-26 15:22:23 +0300

    add surf slider

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,21 @@ window.onload = function() {
     infinite: true,
     prevArrow: '<img class="slider__arrows slider__arrows-left" src="src/img/arrow-left.svg" alt="arrow-left">',
     nextArrow: '<img class="slider__arrows slider__arrows-right" src="src/img/arrow-right.svg" alt="arrow-right">',
+    asNavFor: '.dots'
+  });
+
+  //// header  slider  dots
+  $('.dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider'
+  });
+
+  // section surf slider
+  $('.surf__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider__arrows slider__arrows-left" src="src/img/arrow-left.svg" alt="arrow-left">',
+    nextArrow: '<img class="slider__arrows slider__arrows-right" src="src/img/arrow-right.svg" alt="arrow-right">',
   });
 };
