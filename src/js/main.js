bf92bcd165ddb55cb6ceb1c99633ab68fe75c205
Gromov Andrey <gromovandrey1985@gmail.com>
2019-12-02 10:53:50 +0300

    add slider for section travel

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-  // slick  slider
+  //////////////////  header  slider  ////////////////////////
   $('.header__slider').slick({
     infinite: true,
     prevArrow: '<img class="slider__arrows slider__arrows-left" src="src/img/arrow-left.svg" alt="arrow-left">',
@@ -15,11 +15,34 @@ window.onload = function() {
     asNavFor: '.header__slider'
   });
 
-  // section surf slider
+
+//////////////////  section surf  ////////////////////////
+//// slider map
+   $('.slider__map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf__slider',
+    focusOnSelect: true
+  });
+
+
+  // surf slider
   $('.surf__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
+    asNavFor: '.slider__map',
     prevArrow: '<img class="slider__arrows slider__arrows-left" src="src/img/arrow-left.svg" alt="arrow-left">',
     nextArrow: '<img class="slider__arrows slider__arrows-right" src="src/img/arrow-right.svg" alt="arrow-right">',
   });
+
+
+  //////////////////  section travel   ////////////////////////
+    $('.travel__slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // asNavFor: '.slider__map',
+      prevArrow: '<img class="slider__arrows slider__arrows-left" src="src/img/arrow-left.svg" alt="arrow-left">',
+      nextArrow: '<img class="slider__arrows slider__arrows-right" src="src/img/arrow-right.svg" alt="arrow-right">',
+    });
 };
